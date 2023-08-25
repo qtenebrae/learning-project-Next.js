e57b02711dcde24b9b12689651618ff2f8b70b87
qtenebrae <serega.ivanovv2012@gmail.com>
2023-08-25 16:07:51 +0300

    Add fonts and Htag component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Next.js</title>
+				<link key={1} rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
