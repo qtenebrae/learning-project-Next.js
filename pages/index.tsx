0ee1de3abfb6c13782e3b3819261a82b5cae6854
qtenebrae <serega.ivanovv2012@gmail.com>
2023-08-29 15:47:01 +0300

    Add component Button
    Using the classnames library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from '@/components';
+import { Button, Htag } from '@/components';
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag="h1">Курсы по Photoshop</Htag>
+			<Button appearance="primary">Кнопка</Button>
+			<Button appearance="ghost">Кнопка</Button>
 		</>
 	);
 }
