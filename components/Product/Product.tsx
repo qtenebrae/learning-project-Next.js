1041d9e47e6580e6b67f2fd143174e8e12ed3050
qtenebrae <serega.ivanovv2012@gmail.com>
2023-09-09 16:04:48 +0300

    Implement adaptive layout of the product component

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,14 +1,20 @@
-import { priceRu } from '@/helpers/helpers';
+import { declOfNum, priceRu } from '@/helpers/helpers';
 import { Button, Card, Divider, Rating, Tag } from '..';
 import styles from './Product.module.css';
 import { ProductProps } from './Product.props';
 import cn from 'classnames';
+import Image from 'next/image';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
 			<div className={styles.logo}>
-				<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+				<Image
+					src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+					alt={product.title}
+					width={70}
+					height={70}
+				/>
 			</div>
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
@@ -34,10 +40,20 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 			</div>
 			<div className={styles.priceTitle}>Цена</div>
 			<div className={styles.creditTitle}>Кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+			<div className={styles.rateTitle}>
+				{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+			</div>
 			<Divider className={styles.hr} />
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
+			<div className={styles.feature}>
+				{product.characteristics.map((c) => (
+					<div className={styles.characteristics} key={c.name}>
+						<span className={styles.characteristicsName}>{c.name}</span>
+						<span className={styles.characteristicsDots}></span>
+						<span className={styles.characteristicsValue}>{c.value}</span>
+					</div>
+				))}
+			</div>
 			<div className={styles.advBlock}>
 				{product.advantages && (
 					<div className={styles.advantages}>
@@ -52,7 +68,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					</div>
 				)}
 			</div>
-			<Divider className={styles.hr} />
+			<Divider className={cn(styles.hr, styles.hr2)} />
 			<div className={styles.actions}>
 				<Button appearance="primary">Узнать подробнее</Button>
 				<Button appearance="ghost" arrow="right" className={styles.reviewButton}>
